4574f802f83e04523cd72a789e531916421250df
Instinct29 <89262248+Instinct29@users.noreply.github.com>
2022-02-24 00:05:56 +0530

    Mid commit
    -Logic part almost done-
    -getting object for all input fields
    -Validating form done
    -style remaining

--- job-posting/src/components/Jobpost.js
+++ job-posting/src/components/Jobpost.js
@@ -1,17 +1,150 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 function Jobpost(){
+
+   const initialValues = { 
+       JobTitle : "",
+       Location : "",
+       jobdescription : "",
+       Experience :"",
+       Category : "",
+       FunctionalArea :"",
+       GraduatingYearStart:"",
+       GraduatingYearEnd:""
+
+   }
+
+   const [formValues, setFormValues] = useState(initialValues);
+   const [formErrors, setFormErrors] = useState({});
+   const [isSubmit, setIsSubmit] = useState(false);
+
+   function changeHandler(e){
+       const{name,value} = e.target
+       setFormValues({...formValues,[name]:value})
+
+   }
+
+
+   function submitHandler(e){
+       e.preventDefault();
+       setFormErrors(validate(formValues));
+       setIsSubmit(true);
+   }
+
+   useEffect(()=>{
+       console.log(formErrors);
+       if (Object.keys(formErrors).length=== 0 && isSubmit){
+              console.log(formValues);
+       }
+   },[formErrors])
+
+
+   const validate = (values)=>{
+       const errors  = {}
+    
+       if (!values.JobTitle){
+           errors.Jobtitle = "Job title is required!"
+       }
+       if (!values.Location){
+           errors.Location = "Location is required!"
+       }
+       if(!values.jobdescription){
+           errors.jobdescription = "This field is required!"
+       }
+
+       return errors;
+
+   }
+
     return(<>
        <article>
        <h1>Basic Details</h1>
        <hr />
        </article>
 
-       <form action="">
-           <label htmlFor="ipt">job Title</label>
-           <input type="text" />
+       <form action="" onSubmit={submitHandler}>
+           <label>Job Title    </label>
+           <input type="text" 
+                  placeholder="Title of your Job Profile"
+                  name="JobTitle"
+                  value={formValues.JobTitle}
+                  onChange={changeHandler}
+           />
+           <p>{formErrors.Jobtitle}</p>
+           <br/>
+
+           <label>Location</label>
+           <input 
+               type="text"
+               name="Location"
+               value={formValues.Location}
+               onChange={changeHandler}
+           />
+           <p>{formErrors.Location}</p>
+
+           <br/>
+
+           <label >Job Description</label>
+           <textarea name="" id="" cols="30" rows="10" name="jobdescription" value={formValues.jobdescription} onChange={changeHandler}></textarea>
+           <p>{formErrors.jobdescription}</p>
+           <br />
+
+           <label>Years of experience</label>
+           <select name="Experience" id="" onChange={changeHandler} >
+               <option value="2 months">0 Months - 1 Year</option>
+               <option value="3 months">More than 1 Year</option>
+               <option value="3 months">More than 5 Year</option>
+               <option value="3 months">More than 10 Year</option>
+               <option value="3 months">Expert</option>
+           </select>
+           <br />
+
+   
+
+           <article> 
+           <h1>Targeting</h1>
+           <hr />
+           </article>
+
+           <label >Category</label>
+           <select name="Category" id="" onChange={changeHandler}>
+               <option value="SDE">SDE</option>
+               <option value="Testing engineer">Testing engineer</option>
+               <option value="Data science">Data science</option>
+               <option value="Algorithm Dev">Algorithm Developer</option>
+               <option value="ML/AI">ML/AI</option>
+           </select>
+
+           <label >FunctionalArea</label>
+           <select name="Functional Area" id="" onChange={changeHandler}>
+               <option value="frontend">Frontend</option>
+               <option value="backend">Backend</option>
+               <option value="fullstack">FullStack</option>
+           </select>
+           <br />
+           
+           <label >Graduating Year| Start</label>
+           <input type="date" name="GraduatingYearStart" onChange={changeHandler}/>
+
+           <br />
+
+           <label >Graduating Year| End</label>
+           <input type="date" name="GraduatingYearEnd" onChange={changeHandler}/>
+
+    
+           <br />
+
+        
+
+           <button >Submit</button>
+           <br />
+           <hr />
+        
        </form>
-       <h2 className="test">my name is manthan gour</h2>
+
+       <pre>{JSON.stringify(formValues, undefined,2)}</pre>
+       
+      
         
 
     </>);
